76d896ea9ab44ec30ad2a8f78f6a934d3716c431
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-10-29 00:29:51 +0530

    L37 Flux range

---
+++ src/main/java/com/rp/sec02/L05FluxRange.java
@@ -0,0 +1,17 @@
+package src.main.java.com.rp.sec02;
+
+import reactor.core.publisher.Flux;
+import src.main.java.com.rp.courseUtil.Util;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+
+public class L05FluxRange {
+    public static void main(String[] args) {
+        Flux.range(1,5)
+                .map( i -> Util.faker().name().fullName())
+                .subscribe(
+                Util.onNext()
+        );
+    }
+}
