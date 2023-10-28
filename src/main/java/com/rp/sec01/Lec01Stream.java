d47c9e7938a0cdae7b6bcfa22671aebce4e2a903
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-10-28 10:13:55 +0530

    L-14 Mono just

--- src/main/java/com/rp/sec01/Lec01Stream.java
+++ src/main/java/com/rp/sec01/Lec01Stream.java
@@ -1,2 +1,19 @@
-package src.main.java.com.rp.sec01;public class Lec01Stream {
+package src.main.java.com.rp.sec01;
+
+import java.util.stream.Stream;
+
+public class Lec01Stream {
+    public static void main(String[] args) {
+        Stream<Integer> stream = Stream.of(1).map(i-> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return i*2;
+        });
+
+//        System.out.println(stream);
+        stream.forEach(System.out::println);
+    }
 }
