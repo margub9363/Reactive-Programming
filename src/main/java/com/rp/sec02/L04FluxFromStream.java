95474bc4be3b93fe7246e889799632a02fd3153e
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-10-29 00:00:21 +0530

    L36 To show that Flux from Stream cannot have multiple subscribers

---
+++ src/main/java/com/rp/sec02/L04FluxFromStream.java
@@ -0,0 +1,30 @@
+package src.main.java.com.rp.sec02;
+
+import reactor.core.publisher.Flux;
+import src.main.java.com.rp.courseUtil.Util;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class L04FluxFromStream {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8);
+        Stream<Integer> stream = list.stream();
+//        stream.forEach(System.out::println);
+//        stream.forEach(System.out::println);  you cannot use the same stream again
+        Flux<Integer> integerFlux = Flux.fromStream(stream);
+        integerFlux
+                .subscribe(
+                        Util.onNext(),
+                        Util.onError(),
+                        Util.onComplete()
+                );
+        integerFlux
+                .subscribe(
+                        Util.onNext(),
+                        Util.onError(),
+                        Util.onComplete()
+                );
+
+    }
+}
