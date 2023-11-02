3a8843669a71b6ec877ebb1efbcccbf70694c84f
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-11-02 13:32:05 +0530

    L78 FlatMap

---
+++ src/main/java/com/rp/sec04/helper/User.java
@@ -0,0 +1,19 @@
+package src.main.java.com.rp.sec04.helper;
+
+import lombok.Data;
+import lombok.ToString;
+import src.main.java.com.rp.courseUtil.Util;
+
+@Data
+@ToString
+public class User {
+
+    private int userId;
+    private String name;
+
+    public User(int userId) {
+        this.userId = userId;
+        this.name = Util.faker().name().fullName();
+    }
+
+}
