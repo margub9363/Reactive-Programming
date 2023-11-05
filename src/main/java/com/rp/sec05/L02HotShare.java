6cea73fefca9583f006d9c471cae720b009c338b
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-11-05 22:44:44 +0530

    L02 Hot Subscribers

---
+++ src/main/java/com/rp/sec05/L02HotShare.java
@@ -0,0 +1,36 @@
+package src.main.java.com.rp.sec05;
+
+import reactor.core.publisher.Flux;
+import src.main.java.com.rp.courseUtil.Util;
+
+import java.time.Duration;
+import java.util.stream.Stream;
+
+public class L02HotShare {
+
+    public static void main(String[] args) {
+        Flux<String> movieStream = Flux.fromStream(() -> getMovie())
+                .delayElements(Duration.ofSeconds(2))
+                .share();
+
+        movieStream.subscribe(Util.subscriber("sam"));
+        Util.sleepSeconds(5);
+        movieStream.subscribe(Util.subscriber("mike"));
+
+        Util.sleepSeconds(60);
+    }
+
+    private static Stream<String > getMovie() {
+        System.out.println("Got the Movie Streaming Request");
+        return Stream.of(
+                "Scene1",
+                "Scene 2",
+                "Scene 3",
+                "Scene 4",
+                "Scene 5",
+                "Scene 6",
+                "Scene 7"
+        );
+    }
+
+}
