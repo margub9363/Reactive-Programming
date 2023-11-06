3bec57082ef30aa7a3c246481242d94570f8f303
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-11-06 23:40:21 +0530

    L94 Subscribe On - Demo

---
+++ src/main/java/com/rp/sec06/L02SubscribeOnDemo.java
@@ -0,0 +1,32 @@
+package src.main.java.com.rp.sec06;
+
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+import src.main.java.com.rp.courseUtil.Util;
+
+public class L02SubscribeOnDemo {
+    public static void main(String[] args) {
+
+        Flux<Object> flux = Flux.create(fluxSink -> {
+            printThreadName("create");
+            fluxSink.next(1);
+        }).doOnNext(i -> printThreadName("next " + i));
+
+        Runnable runnable = () -> flux
+                .doFirst(() -> printThreadName("first2"))
+                .subscribeOn(Schedulers.boundedElastic())
+                .doFirst( () -> printThreadName("first1"))
+                .subscribe(v-> printThreadName("sub " + v));
+
+        for (int i = 0; i < 2; i++) {
+            new Thread(runnable).start();
+
+        }
+
+        Util.sleepSeconds(5);
+    }
+
+    private static void printThreadName(String msg) {
+        System.out.println(msg + "\t\t: Thread : "+ Thread.currentThread().getName());
+    }
+}
