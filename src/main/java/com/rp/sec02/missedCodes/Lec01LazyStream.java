95526ed24a68bc299822aa101e28762b836e5150
margub9363 <69838623+margub9363@users.noreply.github.com>
2025-12-22 13:02:27 +0530

    Section2 L16

--- src/main/java/com/rp/sec02/missedCodes/Lec01LazyStream.java
+++ src/main/java/com/rp/sec02/missedCodes/Lec01LazyStream.java
@@ -1,2 +1,14 @@
-package com.rp.sec02.missedCodes;public class Lec01LazyStream {
+package com.rp.sec02.missedCodes;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Slf4j
+public class Lec01LazyStream {
+    public static void main(String[] args) {
+        Stream.of(1).peek(i -> log.info("received : {}", i)); // << this is not going to given any output because of Lazy
+        Stream.of(1).peek(i -> log.info("received : {}", i)).collect(Collectors.toList()); // <<this gives output
+    }
 }
