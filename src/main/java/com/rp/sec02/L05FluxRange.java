4218b63aeb289ddb24276c59267a1b8623b65a90
margub9363 <69838623+margub9363@users.noreply.github.com>
2025-12-23 18:07:48 +0530

    Sec01 L05 Flux Range - generating 5 different random names

--- src/main/java/com/rp/sec02/L05FluxRange.java
+++ src/main/java/com/rp/sec02/L05FluxRange.java
@@ -8,15 +8,15 @@ import java.util.function.Consumer;
 
 public class L05FluxRange {
     public static void main(String[] args) {
-        Flux.range(3,10)
+       /* Flux.range(3,10)
                 .subscribe(
                         Util.onNext()
-                );
-        /*Flux.range(1,5)
-                .log()
+                );*/
+        Flux.range(1,5)
+//                .log()
                 .map( i -> Util.faker().name().fullName())
                 .subscribe(
                 Util.onNext()
-        );*/
+        );
     }
 }
