016123558fdd2fb303693ce7dc1b556004226cfa
margub9363 <69838623+margub9363@users.noreply.github.com>
2025-12-23 18:58:19 +0530

    Sec01 L46 Flux vs list

--- src/main/java/com/rp/sec02/L07FluxVsList.java
+++ src/main/java/com/rp/sec02/L07FluxVsList.java
@@ -1,14 +1,16 @@
 package src.main.java.com.rp.sec02;
 
+import lombok.extern.slf4j.Slf4j;
 import src.main.java.com.rp.courseUtil.Util;
 import src.main.java.com.rp.sec02.helper.NameGenerator;
 
 import java.util.List;
 
+@Slf4j
 public class L07FluxVsList {
     public static void main(String[] args) {
-//        List<String> names = NameGenerator.getNames(5);
-//        System.out.println(names);
+        List<String> names = NameGenerator.getNameList(5);
+        log.info(names.toString());
 
         NameGenerator.getNames(5)
                 .subscribe(Util.onNext());
--- src/main/java/com/rp/sec02/helper/NameGenerator.java
+++ src/main/java/com/rp/sec02/helper/NameGenerator.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class NameGenerator {
 
-/*    public static List<String> getNames(int count) {
+    public static List<String> getNameList(int count) {
         List<String > list = new ArrayList<>(count);
         for (int i = 0; i<count; i++){
             list.add(getNames());
@@ -16,7 +16,6 @@ public class NameGenerator {
         return list;
     }
 
- */
     private static String getNames(){
         Util.sleepSeconds(1);
         return Util.faker().name().fullName();
