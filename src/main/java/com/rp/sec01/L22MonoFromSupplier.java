a42f7bf2a183554d6c3fa50bd216578eb49f4e42
margub9363 <69838623+margub9363@users.noreply.github.com>
2025-12-23 09:11:22 +0530

    Sec01 L22 Mono.just

---
+++ src/main/java/com/rp/sec01/L22MonoFromSupplier.java
@@ -0,0 +1,28 @@
+package com.rp.sec01;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Slf4j
+public class L22MonoFromSupplier {
+    public static void main(String[] args) {
+
+        var list = List.of(1,2,3);
+        Mono.just(sum(list));
+
+
+//        .subscribe(src.main.java.com.rp.courseUtil.Util.subscriber());
+
+//        Mono.fromSupplier( () -> sum(list))
+//                .subscribe(src.main.java.com.rp.courseUtil.Util.subscriber());
+    }
+
+    private static int sum(List<Integer> list) {
+        log.info("Finding the sum of {}" ,list);
+        long count = list.stream().mapToInt(value -> value).count();
+        log.info("Count is : {}", count);
+        return (int) count;
+    }
+}
